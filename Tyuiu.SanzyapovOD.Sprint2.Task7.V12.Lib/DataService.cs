935169047462b421bd25b64f71b28131576e7768
Tyuiu.SanzyapovOD@outlook.com <oleg.sanzjapov@mail.ru>
2024-10-15 20:40:08 +0500

    Task7

---
+++ Tyuiu.SanzyapovOD.Sprint2.Task7.V12.Lib/DataService.cs
@@ -0,0 +1,22 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.SanzyapovOD.Sprint2.Task7.V12.Lib
+{
+    public class DataService : ISprint2Task7V12
+    {
+        public bool CheckDotInShadedArea(double x, double y)
+        {
+            bool res;
+            bool upperZone = (y <= Math.Pow(x, 2)) && (y >= 2 - x);
+            bool lowerZone = (y >= 0) && y >= Math.Pow(x, 2) && (y <= 2 - x);
+            if (upperZone && lowerZone)
+            {
+                res = true;
+            }
+            else
+            {
+                res = false;
+            }
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.SanzyapovOD.Sprint2.Task7.V12.Test/DataServiceTest.cs
@@ -0,0 +1,37 @@
+using Tyuiu.SanzyapovOD.Sprint2.Task7.V12.Lib;
+namespace Tyuiu.SanzyapovOD.Sprint2.Task7.V12.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCheckDotInShadedArea1()
+        {
+            DataService ds = new DataService();
+
+            double x = -2;
+            double y = 4;
+
+            bool res = ds.CheckDotInShadedArea(x, y);
+
+            bool wait = true;
+
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void ValidCheckDotInShadedArea2()
+        {
+            DataService ds = new DataService();
+
+            double x = 1;
+            double y = 6;
+
+            bool res = ds.CheckDotInShadedArea(x, y);
+
+            bool wait = false;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
