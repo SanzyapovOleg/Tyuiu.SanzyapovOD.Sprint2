bd4b3e883a59751ce234ab78f8bd104a8f6af85c
Tyuiu.SanzyapovOD@outlook.com <oleg.sanzjapov@mail.ru>
2024-10-15 19:39:08 +0500

    Task4

--- Tyuiu.SanzyapovOD.Sprint2.Task4.V12.Lib/DataService.cs
+++ Tyuiu.SanzyapovOD.Sprint2.Task4.V12.Lib/DataService.cs
@@ -5,9 +5,7 @@ namespace Tyuiu.SanzyapovOD.Sprint2.Task4.V12.Lib
     {
         public double Calculate(double x, double y)
         {
-            double z;
-            z = (Math.Sqrt(x) > (y * 2) ? Math.Pow(7 + (2 / y * y), x) : (3 * Math.Pow(x, 2) - Math.Cos(y * y) + 10)) / (Math.Pow(y, 2) - Math.Sin(x * x) + 12);
-            return Math.Round((z), 3);
+            return Math.Sqrt(x) > y * 2 ? Math.Round(Math.Pow(7 + 2 / Math.Pow(y, 2), x), 3) : Math.Round((3 * Math.Pow(x, 2) - Math.Pow(Math.Cos(y), 2) + 10) / (Math.Pow(y, 2) - Math.Sin(Math.Pow(x, 2)) + 12), 3);
         }
     }
 }
