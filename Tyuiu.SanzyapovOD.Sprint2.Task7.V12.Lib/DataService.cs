594839f39de295565347e59fcbe38f783639981c
Tyuiu.SanzyapovOD@outlook.com <oleg.sanzjapov@mail.ru>
2024-10-15 20:47:02 +0500

    Task7

--- Tyuiu.SanzyapovOD.Sprint2.Task7.V12.Lib/DataService.cs
+++ Tyuiu.SanzyapovOD.Sprint2.Task7.V12.Lib/DataService.cs
@@ -6,9 +6,9 @@ namespace Tyuiu.SanzyapovOD.Sprint2.Task7.V12.Lib
         public bool CheckDotInShadedArea(double x, double y)
         {
             bool res;
-            bool upperZone = (y <= Math.Pow(x, 2)) && (y >= 2 - x);
-            bool lowerZone = (y >= 0) && y >= Math.Pow(x, 2) && (y <= 2 - x);
-            if (upperZone && lowerZone)
+            bool upperZone = (y <= Math.Pow(x, 2)) | (y >= 2 - x);
+            bool lowerZone = (y >= 0) | y >= Math.Pow(x, 2) | (y <= 2 - x);
+            if (upperZone | lowerZone)
             {
                 res = true;
             }
